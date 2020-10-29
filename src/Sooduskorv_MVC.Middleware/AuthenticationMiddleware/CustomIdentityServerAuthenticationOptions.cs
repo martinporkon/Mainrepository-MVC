fb9fa9d84e9305663538ad7e967a96c3e266255b
martinporkon <45212111+martinporkon@users.noreply.github.com>
2020-10-29 20:02:55 +0200

    architecture with folder structure amendments

---
+++ src/Sooduskorv_MVC.Middleware/AuthenticationMiddleware/CustomIdentityServerAuthenticationOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using IdentityServer4.AccessTokenValidation;
+
+namespace Sooduskorv_MVC.Middleware.AuthenticationMiddleware
+{
+    public class CustomIdentityServerAuthenticationOptions
+    {
+        public Action<IdentityServerAuthenticationOptions> configureOptions { get; set; }
+    }
+}

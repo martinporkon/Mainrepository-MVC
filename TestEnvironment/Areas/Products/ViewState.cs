4fd21725cf87477d07904e036bbbcc2d0179511e
martinporkon <45212111+martinporkon@users.noreply.github.com>
2020-12-31 17:47:35 +0200

    save

--- TestEnvironment/Areas/Products/ViewState.cs
+++ TestEnvironment/Areas/Products/ViewState.cs
@@ -6,19 +6,11 @@ namespace TestEnvironment.Areas.Products.Pages
 {
     public class ViewState
     {
-        /*public string SelectedColor { get; private set; }*/
-
-        public event Func<Task> Notify;
         public ProductTypeView Product { get; set; }
 
         public event Action OnChange;
 
         public event Action OnRemove;
-        /*public void SetColor(string color)
-        {
-            SelectedColor = color;
-            NotifyStateChanged();
-        }*/
 
         public void NotifyStateChanged() => OnChange?.Invoke();
         public void NotifyStateRemoveChanged() => OnRemove?.Invoke();

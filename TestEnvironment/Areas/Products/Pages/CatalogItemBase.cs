256595c5ea4002485851b9883b414e05f18fbf93
martinporkon <45212111+martinporkon@users.noreply.github.com>
2020-12-31 18:39:59 +0200

    changed code.

--- TestEnvironment/Areas/Products/Pages/CatalogItemBase.cs
+++ TestEnvironment/Areas/Products/Pages/CatalogItemBase.cs
@@ -6,29 +6,6 @@ namespace TestEnvironment.Areas.Products.Pages
 {
     public class CatalogItemBase : ComponentBase
     {
-        [Parameter]
-        public ProductTypeView Product { get; set; }
 
-        [Parameter]
-        public EventCallback<ProductTypeView> OnProductSelected { get; set; }
-        [Parameter]
-        public EventCallback<ProductTypeView> OnProductRemoved { get; set; }
-        public async Task AddToBasketChanged()
-        {
-            Product.ProductsInUserBasket++;
-            await OnProductSelected.InvokeAsync(Product);
-            this.StateHasChanged();
-        }
-
-        public async Task RemoveBasketChanged()
-        {
-            var count = Product.ProductsInUserBasket--;
-            if (count < 0)
-            {
-                Product.ProductsInUserBasket = 0;
-            }
-            await OnProductRemoved.InvokeAsync(Product);
-            this.StateHasChanged();
-        }
     }
 }
--- TestEnvironment/Areas/Products/Pages/DisplayBasketItemBase.cs
+++ TestEnvironment/Areas/Products/Pages/DisplayBasketItemBase.cs
@@ -7,55 +7,6 @@ namespace TestEnvironment.Areas.Products.Pages
 {
     public class DisplayBasketItemBase : ComponentBase
     {
-        [Parameter]
-        public IEnumerable<ProductTypeView> Products { get; set; }
-
-        [Parameter]
-        public bool Show { get; set; }
-
-        [Parameter]
-        public EventCallback<int> OnBasketChanged { get; set; }
-
-        [Inject]
-        public ViewState ViewState { get; set; }
-
-        /*protected int SelectedItemsCount { get; set; } = 0;
-        protected void BasketItemChanged(bool status)
-        {
-            if (status)
-            {
-                SelectedItemsCount++;
-            }
-            else
-            {
-                if (SelectedItemsCount > 0)
-                {
-                    SelectedItemsCount--;
-                }
-            }
-
-        }*/
-        private int ProductCount;
-
-        //TODO ostukorvi lisatakase vale toode?
-
-        protected async Task ProductSelectionChanged(ProductTypeView Product)
-        {
-            this.ProductCount = Product.ProductsInUserBasket;
-            ViewState.Product = Product;
-            ViewState.NotifyStateChanged();
-        }
-
-        protected async Task ProductSelectionRemovedChanged(ProductTypeView Product)
-        {
-            this.ProductCount = Product.ProductsInUserBasket;
-            ViewState.Product = Product;
-            ViewState.NotifyStateRemoveChanged();
-        }
-
-        /*public async Task OnNotify()
-        {
-            await InvokeAsync(StateHasChanged);
-        }*/
+       
     }
 }

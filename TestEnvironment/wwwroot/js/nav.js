1b862fa2eb8e3095ee97dd5f05f26276ebb5d501
Ürgen Sõukand <ursouk@ttu.ee>
2020-12-31 16:18:22 +0200

    categooriate kuvamine toimib, vaja koodi

--- TestEnvironment/wwwroot/js/nav.js
+++ TestEnvironment/wwwroot/js/nav.js
@@ -2,22 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-<script src="_framework/blazor.server.js"></script>
+//<script src="_framework/blazor.server.js"></script>
 
-    <script>
-            /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-        var dropdown = document.getElementsByClassName("dropdown-btn");
-        var i;
+   
+    var acc = document.getElementsByClassName("esh-catalog-accordion");
+    var i;
 
-            for (i = 0; i < dropdown.length; {
-            dropdown[i].addEventListener("click", function () {
-                this.classList.toggle("active");
-                var dropdownContent = this.nextElementSibling;
-                if (dropdownContent.style.display === "block") {
-                    dropdownContent.style.display = "none";
-                } else {
-                    dropdownContent.style.display = "block";
-                }
-            });
-            }
-        </script>
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}

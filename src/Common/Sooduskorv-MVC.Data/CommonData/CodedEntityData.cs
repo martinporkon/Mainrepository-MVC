a349876c3df18aa7e0b79f037505cbe6b9aaa672
Ürgen Sõukand <ursouk@ttu.ee>
2021-01-01 21:14:33 +0200

    sidebar ise funktsioneerib, aga pole querysid mis tooks õiged andmed

---
+++ src/Common/Sooduskorv-MVC.Data/CommonData/CodedEntityData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonData
+{
+    public abstract class CodedEntityData : NamedEntityData
+    {
+        public string Code { get; set; }
+    }
+}
--- src/Common/Sooduskorv-MVC.Data/CommonData/NamedEntityData.cs
+++ src/Common/Sooduskorv-MVC.Data/CommonData/NamedEntityData.cs
@@ -2,7 +2,6 @@
 {
     public abstract class NamedEntityData : PeriodData
     {
-        public string Code { get; set; }
         public string Name { get; set; }
     }
 }

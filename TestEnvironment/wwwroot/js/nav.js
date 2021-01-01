3aa647c9cde8af272d1435aa66e20e8243e88167
Regina Waren <reware@ttu.ee>
2021-01-01 23:54:54 +0200

    Muudatused kategooriate menüüribas

--- TestEnvironment/wwwroot/js/nav.js
+++ TestEnvironment/wwwroot/js/nav.js
@@ -1,46 +1,38 @@
 ﻿
-//<script src="_framework/blazor.server.js"></script>
 
-   
-//    var acc = document.getElementsByClassName("esh-catalog-accordion");
-//    var i;
+//var acc = document.getElementsByClassName("esh-catalog-accordion");
+//var i;
 
 //for (i = 0; i < acc.length; i++) {
-//    acc[i].addEventListener("click", function () {
-//        /* Toggle between adding and removing the "active" class,
-//        to highlight the button that controls the panel */
-//        this.classList.toggle("active");
+//    acc[i].addEventListener("click",
+//        function() {
+//            /* Toggle between adding and removing the "active" class,
+//            to highlight the button that controls the panel */
+//            this.classList.toggle("active");
 
-//        /* Toggle between hiding and showing the active panel */
-//        var panel = this.nextElementSibling;
-//        if (panel.style.display === "block") {
-//            panel.style.display = "none";
-//        } else {
-//            panel.style.display = "block";
-//        }
-//    });
+//            /* Toggle between hiding and showing the active panel */
+//            var panel = this.nextElementSibling;
+//            if (panel.style.display === "block") {
+//                panel.style.display = "none";
+//            } else {
+//                panel.style.display = "block";
+//            }
+//        });
 //}
-
-var acc = document.getElementsByClassName("esh-catalog-accordion");
+  
+var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click",
-        function() {
-            /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
-            this.classList.toggle("active");
-
-            /* Toggle between hiding and showing the active panel */
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
 }
-  
-
 
 

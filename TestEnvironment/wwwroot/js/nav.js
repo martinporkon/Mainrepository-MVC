9f8224ce214df40b4bde70933d79fe622e109087
Ürgen Sõukand <ursouk@ttu.ee>
2021-01-02 23:12:34 +0200

    regina asjad mis mergest ära kustusid tagasi pandud

--- TestEnvironment/wwwroot/js/nav.js
+++ TestEnvironment/wwwroot/js/nav.js
@@ -1,25 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
-// Write your JavaScript code.
-//<script src="_framework/blazor.server.js"></script>
-
-   
-    var acc = document.getElementsByClassName("esh-catalog-accordion");
-    var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
         this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
         } else {
-            panel.style.display = "block";
+            dropdownContent.style.display = "block";
         }
     });
 }
